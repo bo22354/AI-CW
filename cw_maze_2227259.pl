1bbc6e6f9a25d168a9e555c57eb3eb420ac711ca
Jackwayt <bo22354@bristol.ac.uk>
2024-12-04 18:50:41 +0000

    PArt 3 Done

--- cw_maze_2227259.pl
+++ cw_maze_2227259.pl
@@ -4,56 +4,48 @@ agent_at_exit([Agent|Rest], Target) :- % Look at each agent and check if at the 
     get_agent_position(Agent, Pos), 
     (Pos = Target -> 
         % If agent is at the target, succeed and cut
-        print_debug("Agent at target", Agent),
-        leave_maze(Agent),
-        % Need to make a function then to go through each agent and find shortest path to exit and then go to it
-        !, 
-        all_exit(Target),
+        leave_maze(Agent), !, 
+        all_exit(Target), % Function for all agents to leave maze
         true
     ;
         % Otherwise, check the remaining agents
         agent_at_exit(Rest, Target)
     ).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 all_exit(Target) :-
     my_agents(Agents),
     maplist(get_agent_position, Agents, Positions),
     Task = go(Target),
 
-    findall([TotalCost, 0, [Pos]],
+    findall([TotalCost, 0, [Pos]], % Create a queue of all the remaining agents
         (
             member(Pos, Positions),
             map_distance(Pos, Target, TotalCost)
         ),
         Queue),
-    sort(Queue, SortedQueue),
+    sort(Queue, SortedQueue), % Sort by the distance to the exit
     all_exit(SortedQueue, Task, Target).
     
 all_exit([], _, _) :- true.
 
 all_exit(SortedQueue, Task, Target) :-   
-    solve_task_aStar_NoE(Task, SortedQueue, [], 0, Path),
-    print_debug("Path", Path),
+    % Find the shortest path from the exit to an agent and then get them to leave the maze 
+    solve_task_aStar_NoE(Task, SortedQueue, [], 0, Path), 
     reverse(Path, RevPath),
-    print_debug("Here", ""),
     RevPath = [AgentLoc | Rest],
-    print_debug("Here", ""),
     append(Rest, [Target], FullPath),
-    print_debug("AgentLoc", AgentLoc),
-    print_debug("FullPath", FullPath),
-
     lookup_pos(AgentLoc, Agent),
-    print_debug("Agent", Agent),
     Agent = a(X),
     agent_do_moves(X, FullPath),
     Remove = [_,_,[AgentLoc]],
     exclude(=(Remove),SortedQueue,NewSortedQueue),
     leave_maze(X),
-    print_debug("New Queue", NewSortedQueue),
     all_exit(NewSortedQueue, Task, Target).
 
-    % Need to find out which agents path it is and then make them do the moves and then recursively call until no asgents left
 
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 solve_maze :-
     my_agents(Agents),
     solve_maze([], [], Agents).
@@ -66,45 +58,31 @@ solve_maze(_, _, Agents) :- % Initial call to solve_maze
     agent_at_exit(Agents, Target).
 
 solve_maze(CompletedNodes, DecisionNodes, Agents) :- % Recursive call for solve_maze
-    % Agents = [A|_],
-    print_debug("---------------------------------------------------------",""),
     agents_moves(Agents, DecisionNodes, CompletedNodes, Moves, NewDecisionNodes, NewCompletedNodes),
-    % find_moves(A, DecisionNodes, CompletedNodes, Moves, NewDecisionNodes, NewCompletedNodes),
-    print_debug("Agents Moves", Moves),
-
     agents_do_moves(Agents,Moves),
     solve_maze(NewCompletedNodes, NewDecisionNodes, Agents).
     
-agents_moves([], DecisionNodes, CompletedNodes, Moves, DecisionNodes, CompletedNodes):- 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+agents_moves([], DecisionNodes, CompletedNodes, Moves, DecisionNodes, CompletedNodes):-  % Base case
     Moves = [].
 
 agents_moves([Agent|Rest], DecisionNodes, CompletedNodes, Moves, NewDecisionNodes, NewCompletedNodes) :-
-    find_moves(Agent, DecisionNodes, CompletedNodes, Move, TempNewDecisionNodes, TempNewCompletedNodes),
-    agents_moves(Rest, TempNewDecisionNodes, TempNewCompletedNodes, RestMoves, NewDecisionNodes, NewCompletedNodes),
-    Moves = [Move | RestMoves],
-    print_debug("CompletedNodes", CompletedNodes),
-    print_debug("DecisionNodes", DecisionNodes).
+    find_moves(Agent, DecisionNodes, CompletedNodes, Move, TempNewDecisionNodes, TempNewCompletedNodes), % Gets the next move for the current agent at the fron of the list
+    agents_moves(Rest, TempNewDecisionNodes, TempNewCompletedNodes, RestMoves, NewDecisionNodes, NewCompletedNodes), %Recursively calls itself to perform same thing on all agents
+    Moves = [Move | RestMoves]. % Compiles all the agents move into one list
 
 
 
-
-%%%%%%%%%%%%%%%% USEFUL PREDICATES %%%%%%%%%%%%%%%%%%
-% Find a possible move for each agent
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 find_moves([],[]).
 
 find_moves(A, DecisionNodes, CompletedNodes, Move, NewDecisionNodes, NewCompletedNodes) :-
     ailp_grid_size(GridSize),
     Target = p(GridSize, GridSize),
     get_agent_position(A, Pos),
-    print_debug("Agent", A),
-    print_debug("Agent Position", Pos),
-    print_debug("CompletedNodes", CompletedNodes),
-    print_debug("DecisionNodes", DecisionNodes),
-    print_debug("", ""),
 
-
-
-    findall([Cost, NewPos],
+    findall([Cost, NewPos], % Gets all the possible moves that are NEW
             (
                 agent_adjacent(A,NewPos, empty),
                 \+ member(NewPos, CompletedNodes),
@@ -113,11 +91,10 @@ find_moves(A, DecisionNodes, CompletedNodes, Move, NewDecisionNodes, NewComplete
 
             ),
             PosMoves),
-    print_debug("PosMoves", PosMoves),
-    length(PosMoves, NumberOfMoves),
-    (NumberOfMoves > 1 ->
 
-        print_debug("Multiple Moves", ""),
+    length(PosMoves, NumberOfMoves),
+    (NumberOfMoves > 1 -> % If we have more then 1 move this means where we are at this point ios a decision node
+        % As its a decision node this means that its not done with yet and will need to be returned to for further exploring
         sort(PosMoves, SortedMoves),
         NewDecisionNodes = [Pos|DecisionNodes],
 
@@ -127,8 +104,7 @@ find_moves(A, DecisionNodes, CompletedNodes, Move, NewDecisionNodes, NewComplete
         M = [Move|_],
         true
     ;
-    NumberOfMoves = 1 ->
-        print_debug("1 Move", ""),
+    NumberOfMoves = 1 -> % Only 1 move meaning that once we move off it theres no more exploring for it
         (member(Pos, DecisionNodes) -> % If on a pre existing decision node with only 1 option then remove it from DecsionNodes and add to CompletedNodes 
             exclude(=(Pos), DecisionNodes, NewDecisionNodes)
         ;
@@ -140,9 +116,8 @@ find_moves(A, DecisionNodes, CompletedNodes, Move, NewDecisionNodes, NewComplete
         M = [Move|_],
         true
     ;
-    % Find the shortest path to Pos when passing in the queue of all the current decision nodes
-        print_debug("No Moves", ""),
-        (DecisionNodes \= [] -> 
+    % No moves so needs to go to a decison node if one is available
+        (DecisionNodes \= [] ->  % If there are available decision nodes then we want to create a queu of them to find which is closest
             findall([TotalCost, 0, [DNode]],
                 (
                     member(DNode, DecisionNodes),
@@ -157,12 +132,9 @@ find_moves(A, DecisionNodes, CompletedNodes, Move, NewDecisionNodes, NewComplete
 
             (solve_task_aStar_NoE(Task, SortedQueue, [], A, Path) ->
                 % If you can find a path then go to it
-                print_debug("PATH", Path),
-
                 agent_do_moves(A, Path),
                 find_moves(A, DecisionNodes, [Pos|CompletedNodes], Move, NewDecisionNodes, NewCompletedNodes)
             ;
-                print_debug("No Path", ""),
                 % If you cant find a path, then theres either no current DecisionNodes or theres an agent blocking the path
                 % Either way just set the move to the current position
                 Move = Pos,
@@ -175,18 +147,12 @@ find_moves(A, DecisionNodes, CompletedNodes, Move, NewDecisionNodes, NewComplete
             Move = Pos,
             NewCompletedNodes = [Pos|CompletedNodes],
             NewDecisionNodes = DecisionNodes   
-        )
-        
-
-        
+        )   
     ).
-    % find_moves(As,Moves). % For doing rest of agents
 
 
-
-
-
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% A* Search as before but without checking for energy levels
 solve_task_aStar_NoE(Task,  [[_, _, [Pos|RPath]]|_], _, _, FinalPath) :- % Check for complete
     achieved(Task, Pos),
     FinalPath = RPath.
@@ -198,7 +164,7 @@ solve_task_aStar_NoE(Task, [[_, PathCost, [Pos|RPath]]|Rest], Visited, Agent, Fi
         [NewTotalCost, NewPathCost, [NewPos,Pos|RPath]], % What Im collecting
         (
             map_adjacent(Pos,NewPos,Obj), 
-            ((Obj = a(Agent), print_debug("Obj", Obj)); Obj = empty),
+            (Obj = a(Agent); Obj = empty),
             \+ memberchk(NewPos,Visited),
             NewPathCost is PathCost + 1,
             man_dist(NewPathCost, NewPos, Tar, NewTotalCost)
